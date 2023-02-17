e69664a9d90cbe4e417feaaabdfc5b01f460c9a7
shaheryar abid <sharyar0310@gmail.com>
2023-02-17 18:29:10 +0500

    Make some changes into the code base

--- src/modules/getLikes.js
+++ src/modules/getLikes.js
@@ -1,26 +1,25 @@
+const API_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const APP_ID = '7P9rWHY7WsKk1rSNM8nF';
+
 const postLike = async (itemId) => {
-  const response = await fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/7P9rWHY7WsKk1rSNM8nF/likes',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        item_id: itemId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  const response = await fetch(`${API_URL}/apps/${APP_ID}/likes`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: itemId,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+  });
   const responseText = await response.text();
   return responseText;
 };
 
 const getLikes = async () => {
-  const response = await fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/7P9rWHY7WsKk1rSNM8nF/likes',
-  );
+  const response = await fetch(`${API_URL}/apps/${APP_ID}/likes`);
   const responseJSON = await response.json();
   return responseJSON;
 };
 
-export { getLikes, postLike };
+export { postLike, getLikes };
+
